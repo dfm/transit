e68f3dce0f86c4da93f35039418278c4268da426
Dan F-M <danfm@nyu.edu>
2014-06-25 08:21:20 -0400

    better building

--- setup.py
+++ setup.py
@@ -1,55 +1,123 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-import sys
+import re
 import os
 
-try:
+
+def find_boost(hint=None):
+    """
+    Find the location of the Boost include directory. This will return
+    ``None`` on failure.
+
+    """
+    # List the standard locations including a user supplied hint.
+    search_dirs = [] if hint is None else [hint]
+    search_dirs += [
+        "/usr/local/include",
+        "/usr/local/homebrew/include",
+        "/opt/local/var/macports/software",
+        "/opt/local/include",
+        "/usr/include",
+        "/usr/include/local",
+    ]
+
+    # Loop over search paths and check for the existence of the Eigen/Dense
+    # header.
+    for d in search_dirs:
+        path = os.path.join(d, "boost", "math", "special_functions",
+                            "ellint_3.hpp")
+        if os.path.exists(path):
+            # Determine the version.
+            vf = os.path.join(d, "boost", "version.hpp")
+            if not os.path.exists(vf):
+                continue
+            src = open(vf, "r").read()
+            v = re.findall("#define BOOST_LIB_VERSION \"(.+)\"", src)
+            if not len(v):
+                continue
+            v = v[0]
+            print("Found Boost version {0} in: {1}".format(v, d))
+            return d
+    return None
+
+if __name__ == "__main__":
+    import sys
+    import numpy
+    import argparse
     from setuptools import setup, Extension
-    setup, Extension
-except ImportError:
-    from distutils.core import setup, Extension
-    setup, Extension
 
-import numpy
+    # Publish the library to PyPI.
+    if "publish" in sys.argv[-1]:
+        os.system("python setup.py sdist upload")
+        sys.exit()
 
+    # Allow the user to specify custom search locations.
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--boost-include", dest="boost",
+                        help="Path to the Boost include directory")
+    args, unknown = parser.parse_known_args()
+    sys.argv = [sys.argv[0]] + unknown
 
-if sys.argv[-1] == "publish":
-    os.system("python setup.py sdist upload")
-    sys.exit()
+    # Find the Boost include directory.
+    boost_include = find_boost(hint=args.boost)
+    if boost_include is None:
+        raise RuntimeError("Required library Boost not found. "
+                           "Try specifying the --boost-include=\"...\" option "
+                           "at the command line.")
 
+    # Choose libraries to link.
+    libraries = []
+    if os.name == "posix":
+        libraries.append("m")
 
-desc = open("README.rst").read()
-required = ["numpy"]
+    # Specify the include directories.
+    include_dirs = [
+        "include",
+        numpy.get_include(),
+        boost_include,
+    ]
 
-include_dirs = [
-    "include",
-    numpy.get_include(),
-]
-src = [
-    "transit/_transit.c",
-    "src/quad.cpp",
-    "src/driver.cpp",
-]
-ext = Extension("transit._transit", src, include_dirs=include_dirs)
+    # The source files.
+    src = [
+        "transit/_transit.c",
+        "src/quad.cpp",
+        "src/driver.cpp",
+    ]
 
-setup(
-    name="transit",
-    version="0.0.2",
-    author="Daniel Foreman-Mackey",
-    author_email="danfm@nyu.edu",
-    packages=["transit"],
-    url="http://github.com/dfm/transit",
-    license="MIT",
-    description="Compute some transits",
-    long_description=desc,
-    install_requires=required,
-    ext_modules=[ext],
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-    ],
-)
+    # Set up the extension.
+    ext = Extension("transit._transit", sources=src,
+                    libraries=libraries, include_dirs=include_dirs)
+
+    # Hackishly inject a constant into builtins to enable importing of the
+    # package before the library is built.
+    if sys.version_info[0] < 3:
+        import __builtin__ as builtins
+    else:
+        import builtins
+    builtins.__TRANSIT_SETUP__ = True
+    import transit
+
+    # Execute the setup command.
+    desc = open("README.rst").read()
+    required = ["numpy"]
+    setup(
+        name="transit",
+        version=transit.__version__,
+        author="Daniel Foreman-Mackey",
+        author_email="danfm@nyu.edu",
+        packages=["transit"],
+        url="http://github.com/dfm/transit",
+        license="MIT",
+        description="Compute some transits",
+        long_description=desc,
+        install_requires=required,
+        ext_modules=[ext],
+        classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Science/Research",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python",
+        ],
+    )
--- transit/__init__.py
+++ transit/__init__.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from ._transit import ldlc_simple, ldlc_kepler
+__version__ = "0.1.0-dev"
+
+try:
+    __TRANSIT_SETUP__
+except NameError:
+    __TRANSIT_SETUP__ = False
+
+if not __TRANSIT_SETUP__:
+    __all__ = ["ldlc_simple", "ldlc_kepler"]
+    from ._transit import ldlc_simple, ldlc_kepler
