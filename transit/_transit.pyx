9597c3c2a81b4807c06f39afab8b8533af83ead6
Dan F-M <danfm@nyu.edu>
2016-01-28 17:12:26 +0000

    more bodies

--- transit/_transit.pyx
+++ transit/_transit.pyx
@@ -27,6 +27,8 @@ cdef extern from *:
     ctypedef int kepler_3 "5+7*3"
     ctypedef int kepler_4 "5+7*4"
     ctypedef int kepler_5 "5+7*5"
+    ctypedef int kepler_6 "5+7*6"
+    ctypedef int kepler_7 "5+7*7"
 
     ctypedef int ttvfaster_2 "1+7*2"
     ctypedef int ttvfaster_3 "1+7*3"
@@ -177,22 +179,30 @@ cdef class CythonSolver:
             flag = integrator.gradient[kepler_1](
                 <double*>params.data, t.shape[0], <double*>t.data, texp,
                 <double*>lc.data, <double*>gradient.data)
-        # elif n_body == 2:
-        #     flag = integrator.gradient[kepler_2](
-        #         <double*>params.data, t.shape[0], <double*>t.data, texp,
-        #         <double*>lc.data, <double*>gradient.data)
-        # elif n_body == 3:
-        #     flag = integrator.gradient[kepler_3](
-        #         <double*>params.data, t.shape[0], <double*>t.data, texp,
-        #         <double*>lc.data, <double*>gradient.data)
-        # elif n_body == 4:
-        #     flag = integrator.gradient[kepler_4](
-        #         <double*>params.data, t.shape[0], <double*>t.data, texp,
-        #         <double*>lc.data, <double*>gradient.data)
-        # elif n_body == 5:
-        #     flag = integrator.gradient[kepler_5](
-        #         <double*>params.data, t.shape[0], <double*>t.data, texp,
-        #         <double*>lc.data, <double*>gradient.data)
+        elif n_body == 2:
+            flag = integrator.gradient[kepler_2](
+                <double*>params.data, t.shape[0], <double*>t.data, texp,
+                <double*>lc.data, <double*>gradient.data)
+        elif n_body == 3:
+            flag = integrator.gradient[kepler_3](
+                <double*>params.data, t.shape[0], <double*>t.data, texp,
+                <double*>lc.data, <double*>gradient.data)
+        elif n_body == 4:
+            flag = integrator.gradient[kepler_4](
+                <double*>params.data, t.shape[0], <double*>t.data, texp,
+                <double*>lc.data, <double*>gradient.data)
+        elif n_body == 5:
+            flag = integrator.gradient[kepler_5](
+                <double*>params.data, t.shape[0], <double*>t.data, texp,
+                <double*>lc.data, <double*>gradient.data)
+        elif n_body == 6:
+            flag = integrator.gradient[kepler_6](
+                <double*>params.data, t.shape[0], <double*>t.data, texp,
+                <double*>lc.data, <double*>gradient.data)
+        elif n_body == 7:
+            flag = integrator.gradient[kepler_7](
+                <double*>params.data, t.shape[0], <double*>t.data, texp,
+                <double*>lc.data, <double*>gradient.data)
         else:
             raise ValueError("You're a maniac. Don't take the gradient of a "
                              "Kepler light curve with more than 5 planets!")
